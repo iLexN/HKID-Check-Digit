174444a6355318c8620ae7f67a43f6f3f6374928
iLexN <ilex.job@gmail.com>
2015-12-04 18:12:10 +0800

    test fix 3

--- tests/Test1.php
+++ tests/Test1.php
@@ -6,12 +6,7 @@ class Test1 extends PHPUnit_Framework_TestCase
      * @dataProvider additionProvider
      */
     public function testCheckHKIDFormat($p1, $p2, $p3)
-    {
-        
-        $this->assertEquals($p1,'B' );
-        $this->assertEquals($p2,'111111' );
-        $this->assertEquals($p3,'1' );
-        
+    {   
         $a = \Ilex\Validation\HkidCheckDigit::checkHKIDFormat($p1, $p2, $p3);
         $this->assertTrue($a);
     }
@@ -20,8 +15,8 @@ class Test1 extends PHPUnit_Framework_TestCase
     {
         return array(
           'B111111(1)' => array('B', '111111', '1'),
-          //'CA182361(1)' => array('C', '182361', '1'),//CA182361(1) 
-          //'ZA182361(3)' => array('ZA', '182361', '3')//ZA182361(3)
+          'CA182361(1)' => array('C', '182361', '1'),
+          'ZA182361(3)' => array('ZA', '182361', '3')
         );
     }
 }
