b207b0cf5916fe1c261e2875e0b224d95c2334ac
iLexN <ilex.job@gmail.com>
2015-12-04 17:59:49 +0800

    fix test

--- tests/Test1.php
+++ tests/Test1.php
@@ -6,7 +6,7 @@ class Test1 extends PHPUnit_Framework_TestCase
     /**
      * @dataProvider additionProvider
      */
-    public function testCheckHKIDFormat($p1, $p2, $p)
+    public function testCheckHKIDFormat($p1, $p2, $p3)
     {
         $a = \Ilex\Validation\HkidCheckDigit::checkHKIDFormat($p1, $p2, $p3);
         $this->assertTrue($a);
