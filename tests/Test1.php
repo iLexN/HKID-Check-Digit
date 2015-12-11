4d7d48cdd3d5876f16c231b14d55a63fd5b7c637
iLexN <ilex.job@gmail.com>
2015-12-11 12:11:45 +0800

    add test

--- tests/Test1.php
+++ tests/Test1.php
@@ -14,9 +14,11 @@ class Test1 extends PHPUnit_Framework_TestCase
     public function additionProvider()
     {
         return array(
-          'B111111(1)' => array('B', '111111', '1'),
-          'CA182361(1)' => array('CA', '182361', '1'),
-          'ZA182361(3)' => array('ZA', '182361', '3')
+            'B111111(1)' => array('B', '111111', '1'),
+            'CA182361(1)' => array('CA', '182361', '1'),
+            'ZA182361(3)' => array('ZA', '182361', '3'),
+            'B111112(A)' => array('B', '111112', 'A'),
+            'B111117(0)' => array('B', '111117', '0'),
         );
     }
 }
