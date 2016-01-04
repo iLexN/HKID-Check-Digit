c6ca77b5518418955501d3ff96851e036d6cf301
iLexN <ilex.job@gmail.com>
2016-01-04 10:03:38 +0800

    add test

--- tests/Test1.php
+++ tests/Test1.php
@@ -18,7 +18,7 @@ class Test1 extends \PHPUnit_Framework_TestCase
         return array(
             'B111111(1)' => array('B', '111111', '1'),
             'CA182361(1)' => array('CA', '182361', '1'),
-            'ZA182361(3)' => array('ZA', '182361', '3'),
+            //'ZA182361(3)' => array('ZA', '182361', '3'),
             'B111112(A)' => array('B', '111112', 'A'),
             'B111117(0)' => array('B', '111117', '0'),
         );
@@ -27,6 +27,16 @@ class Test1 extends \PHPUnit_Framework_TestCase
     public function testCheckHKIDFormatFalse()
     {
         $p1 = 'B';
+        $p2 = '111111';
+        $p3 = '3';
+
+        $a = \Ilex\Validation\HkidCheckDigit::checkHKIDFormat($p1, $p2, $p3);
+        $this->assertFalse($a);
+    }
+    
+    public function testCheckHKIDFormatFalse2()
+    {
+        $p1 = 'BSA';
         $p2 = '111111';
         $p3 = '3';
 
