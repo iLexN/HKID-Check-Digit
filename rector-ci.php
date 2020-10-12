6ca9bf8d8f09f3674f325b76f52d7c59bf2f771c
iLexN <ilex.job@gmail.com>
2020-10-12 11:34:09 +0800

    rector rule update

--- rector-ci.php
+++ rector-ci.php
@@ -4,12 +4,37 @@ declare(strict_types=1);
 
 use Rector\CodingStyle\Rector\Class_\AddArrayDefaultToArrayPropertyRector;
 use Rector\CodingStyle\Rector\Switch_\BinarySwitchToIfElseRector;
+use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
-    $parameters->set('sets', ['code-quality', 'dead-code', 'coding-style', 'php-70', 'php-71', 'php-72', 'php-73', 'php-74', 'solid']);
+    $parameters->set(Option::SETS, [
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::CODING_STYLE,
+        SetList::PHP_74,
+        SetList::SOLID,
+        SetList::CODING_STYLE_ADVANCED,
+        SetList::PERFORMANCE,
+        SetList::PHPUNIT_91,
+        SetList::PHPUNIT_CODE_QUALITY,
+        SetList::PHPUNIT_EXCEPTION,
+        SetList::PHPUNIT_MOCK,
+        SetList::PHPUNIT_INJECTOR,
+        SetList::PHPUNIT_YIELD_DATA_PROVIDER,
+        SetList::PHPUNIT_SPECIFIC_METHOD,
+        SetList::PHPSTAN,
+    ]);
 
-    $parameters->set('exclude_rectors', [BinarySwitchToIfElseRector::class, AddArrayDefaultToArrayPropertyRector::class]);
+    $parameters->set('exclude_rectors', [
+        BinarySwitchToIfElseRector::class,
+        AddArrayDefaultToArrayPropertyRector::class,
+    ]);
+
+    $parameters->set(Option::EXCLUDE_PATHS, [
+        __DIR__ . '/src/Helper.php',
+    ]);
 };
