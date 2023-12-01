6f6852fcad4d954621630797b52e3c4afb3ca8bd
iLexN <ilex.job@gmail.com>
2023-12-01 18:12:28 +0800

    fix rector config

--- rector-ci.php
+++ rector-ci.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\CodingStyle\Rector\Class_\AddArrayDefaultToArrayPropertyRector;
-use Rector\CodingStyle\Rector\Switch_\BinarySwitchToIfElseRector;
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\SetList;
@@ -14,25 +13,15 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::CODING_STYLE,
-        SetList::PHP_74,
-        SetList::PHP_80,
-        SetList::PHP_81,
         SetList::PHP_82,
-        PHPUnitSetList::PHPUNIT_91,
         PHPUnitSetList::PHPUNIT_100,
         PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-        PHPUnitSetList::PHPUNIT_EXCEPTION,
-        PHPUnitSetList::PHPUNIT_EXCEPTION,
-        PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD,
     ]);
 
     $rectorConfig->skip([
-        BinarySwitchToIfElseRector::class,
         AddArrayDefaultToArrayPropertyRector::class,
-        //Rector\Php80\Rector\FunctionLike\UnionTypesRector::class,
         Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector::class,
         Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector::class,
         Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector::class,
-//        __DIR__ . '/src/Helper.php',
     ]);
 };
