03328e3c3e9cf28eefc97c8a8dfb62f146c9508a
iLexN <ilex.job@gmail.com>
2023-12-07 14:50:56 +0800

    php 8.3

--- rector-ci.php
+++ rector-ci.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -12,7 +13,7 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::CODING_STYLE,
-        SetList::PHP_83,
+        LevelSetList::UP_TO_PHP_83,
         PHPUnitSetList::PHPUNIT_100,
         PHPUnitSetList::PHPUNIT_CODE_QUALITY,
     ]);
